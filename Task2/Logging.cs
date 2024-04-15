845bf93ed0efd935bc18154953a7da8728d5c52d
Aderevs <aderevs@gmail.com>
2024-04-15 17:34:22 +0300

    task 2

---
+++ Task2/Logging.cs
@@ -0,0 +1,19 @@
+﻿using Microsoft.AspNetCore.Mvc.Filters;
+using System.Globalization;
+
+namespace Task2
+{
+    public class Logging : ActionFilterAttribute
+    {
+        private const string _logFilePath = @"AppData/log.txt";
+        public override void OnActionExecuting(ActionExecutingContext context)
+        {
+            string name = context.ActionDescriptor.DisplayName;
+            if (!string.IsNullOrEmpty(name))
+            {
+                using StreamWriter streamWriter = new StreamWriter(_logFilePath, true);
+                streamWriter.WriteLine($"Action method: {name} was called at: {DateTime.Now}");
+            }
+        }
+    }
+}
